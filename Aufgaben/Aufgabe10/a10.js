c54f5bf0f307c7170acd914b39a14969133b5fe8
piagiovannelli <101904347+piagiovannelli@users.noreply.github.com>
2022-06-10 14:33:55 +0200

    .

--- Aufgaben/Aufgabe10/a10.js
+++ Aufgaben/Aufgabe10/a10.js
@@ -1,41 +1,28 @@
-var L10;
-(function (L10) {
-    // Variable für die Aufzählung der ToDos
-    let counter = 0;
-    // Variable für den Input
+window.addEventListener("load", function () {
+    let index = 0;
     let input = document.querySelector("#textfield");
-    // Eventlistener für das Hinzufügen einer neuen ToDo
     document.querySelector("#btn").addEventListener("click", newTask);
     function newTask() {
-        // Counter für Anzahl von ToDos
-        counter++;
-        document.querySelector("#todoAmount").innerHTML = String(counter + " in total");
+        index++;
+        document.querySelector("#todoAmount").innerHTML = String(index + " in total");
         let eingabefeld = document.createElement("p");
         eingabefeld.innerHTML = input.value;
-        // Input leeren
-        input.value = " ";
-        //
+        input.value = "";
         let task = document.getElementById("task");
         let check = document.createElement("div");
         let trash = document.createElement("div");
-        /* Neues div für task*/
         let wrapper = document.createElement("div");
         wrapper.textContent = "";
-        // Append Elemente 
         task.appendChild(wrapper);
         wrapper.appendChild(eingabefeld);
         wrapper?.appendChild(check);
         wrapper?.appendChild(trash);
         task?.appendChild(wrapper);
-        // Id bzw. Klassen zuweisung
         eingabefeld.id = "eingabe";
         wrapper.id = "neuesDiv";
         check.className = "far fa-circle";
         trash.className = "fas fa-trash";
-        //
-        /*Eventlistener für die Check(erlefigt)-Box*/
         check.addEventListener("click", erledigteToDos);
-        /* Fkt die ToDo als erledigt markiert - counter wird um 1 erhöht wenn ToDo hinzukommt*/
         function erledigteToDos() {
             if (check.getAttribute("class") == "far fa-check-circle") {
                 check.setAttribute("class", "far fa-circle");
@@ -44,21 +31,15 @@ var L10;
                 check.setAttribute("class", "far fa-check-circle");
             }
         }
-        // 
-        /* Eventlistener für den Mülleimer*/
         trash.addEventListener("click", deleteToDo);
-        /*Fkt die ToDo löscht nach klick auf Mülleimer - counter wird um 1 verringert wenn ToDo gelöscht wird*/
         function deleteToDo() {
             wrapper.parentElement.removeChild(wrapper);
-            counter--;
-            document.querySelector("#todoAmount").innerHTML = String(counter + " in total");
         }
     }
-    // Neue ToDo wird hinzugefügt nach drücken der Entertaste
     document.addEventListener("keydown", function (event) {
         if (event.key == "Enter") {
             newTask();
         }
     });
-})(L10 || (L10 = {}));
+});
 //# sourceMappingURL=a10.js.map
--- Aufgaben/Aufgabe10/a10.ts
+++ Aufgaben/Aufgabe10/a10.ts
@@ -1,81 +1,60 @@
-namespace L10 {
+window.addEventListener("load", function(): void {
+    let index: number = 0;
 
-    // Variable für die Aufzählung der ToDos
-    let counter: number = 0;
-
-    // Variable für den Input
     let input: HTMLInputElement = document.querySelector("#textfield");
 
-
-    // Eventlistener für das Hinzufügen einer neuen ToDo
     document.querySelector("#btn").addEventListener("click", newTask);
 
-
     function newTask (): void {
-
-        // Counter für Anzahl von ToDos
-        counter++; 
-        document.querySelector("#todoAmount").innerHTML = String( counter + " in total");
-
-
+        index++;
+        document.querySelector("#todoAmount").innerHTML = String( index + " in total");
+        
         let eingabefeld: HTMLElement = document.createElement("p");
         eingabefeld.innerHTML = input.value;
 
-        // Input leeren
-        input.value = " ";
+        input.value = "";
 
-        //
         let task: HTMLElement = document.getElementById("task");
         let check: HTMLDivElement = document.createElement("div");
         let trash: HTMLDivElement = document.createElement("div");
-            /* Neues div für task*/ 
         let wrapper: HTMLDivElement = document.createElement("div");
 
         wrapper.textContent = "";
-
-        // Append Elemente 
+ 
         task.appendChild(wrapper);
         wrapper.appendChild(eingabefeld);
         wrapper?.appendChild(check);
         wrapper?.appendChild(trash);    
         task?.appendChild(wrapper);
         
-        // Id bzw. Klassen zuweisung
         eingabefeld.id = "eingabe";
         wrapper.id = "neuesDiv";
         check.className = "far fa-circle";
         trash.className = "fas fa-trash";
 
-        //
-            /*Eventlistener für die Check(erlefigt)-Box*/
+        
         check.addEventListener("click", erledigteToDos);
-            /* Fkt die ToDo als erledigt markiert - counter wird um 1 erhöht wenn ToDo hinzukommt*/
+       
         function erledigteToDos (): void {
             if (check.getAttribute("class") == "far fa-check-circle") {
-                check.setAttribute("class", "far fa-circle"); }
+                check.setAttribute("class", "far fa-circle");
+            }
             else {
                 check.setAttribute("class", "far fa-check-circle"); 
             }    
         }
 
-        // 
-           /* Eventlistener für den Mülleimer*/
         trash.addEventListener("click", deleteToDo);
-            /*Fkt die ToDo löscht nach klick auf Mülleimer - counter wird um 1 verringert wenn ToDo gelöscht wird*/
+        
         function deleteToDo (): void {
-            wrapper.parentElement.removeChild(wrapper); 
-            counter--;
-            document.querySelector("#todoAmount").innerHTML = String( counter + " in total");
+            wrapper.parentElement.removeChild(wrapper);
         }
 
     }
 
-    // Neue ToDo wird hinzugefügt nach drücken der Entertaste
     document.addEventListener("keydown", function (event: KeyboardEvent): void { 
         if (event.key == "Enter") {
         newTask();
         }
     });
-
-
-}
+});
